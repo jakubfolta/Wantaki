f924e9ebaab80db0dc8ef8e5ce096afe69d2e79c
Jakub Folta <jakubfolta@yahoo.co.uk>
2021-02-12 16:29:31 +0000

    Add redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './style/sass/main.scss';
+import { createStore, composeEnhancers } from 'redux';
+
+
 import App from './App';
 
 ReactDOM.render(
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+  token: null,
+  userId: null,
+  error: null
+}
+
+const reducer = (state = initialState, action) => {
+  return state;
+}
+
+export default reducer;
