e15547f166826d6ac39364eba04b791f5fd83dd2
Jakub Folta <jakubfolta@yahoo.co.uk>
2021-02-15 15:09:47 +0000

    Set redux

---
+++ src/store/actions/auth.js
@@ -0,0 +1,24 @@
+import * as actions from './actionTypes';
+
+export const authStart = () => {
+  return {
+    type: actions.AUTH_START
+  }
+}
+
+export const authSuccess = (token, userId, expireTime) => {
+
+}
+
+export const authFail = error => {
+
+}
+
+export const logout = () => {
+
+}
+
+export const auth = (email, password, type) => {
+
+}
+
