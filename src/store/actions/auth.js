8653227d04c839afef2385cc6bfe584a0b78689d
Jakub Folta <jakubfolta@yahoo.co.uk>
2021-02-16 13:34:28 +0000

    Set redux

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -3,22 +3,31 @@ import * as actions from './actionTypes';
 export const authStart = () => {
   return {
     type: actions.AUTH_START
-  }
-}
+  };
+};
 
-export const authSuccess = (token, userId, expireTime) => {
-  
-}
+export const authSuccess = (token, userId) => {
+  return {
+    type: actions.AUTH_SUCCESS,
+    token: token,
+    userId: userId
+  };
+};
 
 export const authFail = error => {
-
-}
+  return {
+    type: actions.AUTH_FAIL,
+    error: error
+  };
+};
 
 export const logout = () => {
-
-}
+  return {
+    type: actions.AUTH_LOGOUT
+  };
+};
 
 export const auth = (email, password, type) => {
 
-}
+};
 
