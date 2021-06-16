f71217ac8d88ee5e65b7afaeda7828c3bead147d
Jakub Folta <jakubfolta@yahoo.co.uk>
2021-06-16 05:33:48 +0100

    Implement delete collection functionality

--- src/components/ListCollections/ListCollection/ListCollection.js
+++ src/components/ListCollections/ListCollection/ListCollection.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaEdit } from 'react-icons/fa';
+import { SiAddthis } from 'react-icons/si';
 
 const ListCollection = props => (
   <div
@@ -9,9 +10,19 @@ const ListCollection = props => (
     onClick={props.handleClick}>
     <h2 className="list_collection_name">{props.name}</h2>
     <button
-      className="list_collection_icon"
+      className="list_collection_icon list_collection_icon--delete"
       onClick={props.handleDelete}>
       <FaTimes />
+    </button>
+    <button
+      className="list_collection_icon list_collection_icon--edit"
+      onClick={props.handleEdit}>
+      <FaEdit />
+    </button>
+    <button
+      className="list_collection_icon list_collection_icon--add"
+      onClick={props.handleAdd}>
+      <SiAddthis />
     </button>
   </div>
 )
