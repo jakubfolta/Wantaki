fb3ce51f02973cafc988dcd36995e19d5d8a066f
Jakub Folta <jakubfolta@yahoo.co.uk>
2021-06-28 14:57:55 +0100

    Style 'collection' items

--- src/components/ListCollections/ListCollections.js
+++ src/components/ListCollections/ListCollections.js
@@ -1,12 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 const ListCollections = props => (
-  <Fragment>
+  <ul className="list list--collections">
     <h2 className="list_title">Collections</h2>
-    <ul className="list list--collections">
-      {props.children}
-    </ul>
-  </Fragment>
+    {props.children}
+  </ul>
 )
 
 export default ListCollections;
