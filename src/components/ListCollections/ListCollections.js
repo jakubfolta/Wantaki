4be6fbf53c8dfe818b42f6ef1d6c80076db2f378
Jakub Folta <jakubfolta@yahoo.co.uk>
2021-07-02 04:53:13 +0100

    Implement 'adding items' to collection functionality

--- src/components/ListCollections/ListCollections.js
+++ src/components/ListCollections/ListCollections.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Spinner from '../UI/Spinner';
 import ListCollection from './ListCollection/ListCollection';
 
-const ListCollections = props => {
-  let items = props.loadingCollections
+class ListCollections extends Component {
+  state = {
+    itemsBoxOpen: false
+  }
+
+  onAddItemsToCollectionHandler = (id) => {
+    this.setState({ itemsBoxOpen: true });
+
+  }
+
+  render() {
+    let items = this.props.loadingCollections
     ? <Spinner />
-    : props.collections.map(el =>
-        <ListCollection
-          id={el.id}
-          key={el.id}
-          // handleClick={}
-          // handleDelete={() => this.onDeleteCollectionHandler(el.id)}
-          // handleCopy={() => this.onCopyCollectionHandler(el.id)}
-          name={el.name}/>
+    : this.props.collections.map(el =>
+      <ListCollection
+        id={el.id}
+        key={el.id}
+        handleAdd={() => this.onAddItemsToCollectionHandler(el.id)}
+        // handleDelete={() => this.onDeleteCollectionHandler(el.id)}
+        // handleCopy={() => this.onCopyCollectionHandler(el.id)}
+        name={el.name}/>
       );
 
-  const collections = props.collections.length > 0
+    const collections = this.props.collections.length > 0
     ? ( <ul className="list list--collections">
           <h2 className="list_title">Collections</h2>
           {items}
         </ul> )
     : null;
 
-  return collections;
+    return collections;
+  }
 }
 
 const mapStateToProps = state => {
